ff0c6f4df26b1db7c525dc170adfe9a1a77f3471
lemuria-pbem <lemuria@online.de>
2021-03-27 14:36:28 +0100

    Improve logging configuration, make throwing exceptions optional.

--- src/AlphaConfig.php
+++ src/AlphaConfig.php
@@ -3,15 +3,26 @@ declare(strict_types = 1);
 namespace Lemuria\Alpha;
 
 use Lemuria\Engine\Fantasya\Storage\LemuriaConfig;
+use Lemuria\Log;
 
 final class AlphaConfig extends LemuriaConfig
 {
 	public const DEBUG_PARTIES = 'debugParties';
 
+	public const THROW_EXCEPTIONS = 'throwExceptions';
+
 	private const DEBUG_PARTIES_DEFAULT = [];
+
+	private const THROW_EXCEPTIONS_DEFAULT = false;
 
 	protected function initDefaults(): void {
 		parent::initDefaults();
-		$this->defaults[self::DEBUG_PARTIES] = self::DEBUG_PARTIES_DEFAULT;
+		$this->defaults[self::DEBUG_PARTIES]    = self::DEBUG_PARTIES_DEFAULT;
+		$this->defaults[self::THROW_EXCEPTIONS] = self::THROW_EXCEPTIONS_DEFAULT;
+	}
+
+	protected function createLog(string $logPath): Log {
+		$addErrorHandler = !$this[self::THROW_EXCEPTIONS];
+		return new AlphaLog($logPath, $addErrorHandler);
 	}
 }
