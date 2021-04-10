db765e98ec2f23afd2ddfe1e515c04c48f44092e
lemuria-pbem <lemuria@online.de>
2021-04-10 14:10:43 +0200

    Add SCSS compilation in Composer update.

---
+++ scripts/compile-report-scss.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+bootstrap=vendor/twbs/bootstrap/dist/css/bootstrap.min.css
+source=web/css/style.scss
+target=web/css/report.min.css
+
+rm -f $target
+cp $bootstrap $target
+echo >> $target
+sass --style compressed --no-cache $source >> $target
