eb40889f4d65d2eff3dce5a02ebb8cff61f43782
lemuria-pbem <lemuria@online.de>
2022-04-15 15:36:36 +0200

    Add keyboard shortcuts to responsive view, content navigation and statistics.

--- web/js/script.js
+++ web/js/script.js
@@ -1,13 +1,28 @@
 /* Lemuria Alpha 0.12 */
 $(function() {
+    const toggleButton = $('#toggle-responsive');
     const toggleClass = 'non-responsive';
+    const navButton = $('#navbar-toggle');
+    const statistics = '#statistics';
 
-    $('#toggle-responsive').click(function () {
+    toggleButton.click(function () {
         let body = $('body');
         if (body.hasClass(toggleClass)) {
             body.removeClass(toggleClass);
         } else {
             body.addClass(toggleClass);
+        }
+    });
+
+    $(document).keydown(function(event) {
+        if (event.key === '#') {
+            toggleButton.click();
+        }
+        if (event.key === 'i') {
+            navButton.click();
+        }
+        if (event.key === 's') {
+            document.location.hash = statistics;
         }
     });
 });
