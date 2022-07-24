b9a4ed6fc84dc76381282fec7e8f83439dac5e8d
lemuria-pbem <lemuria@online.de>
2022-07-24 11:33:23 +0200

    Improve converter for generated maps.

---
+++ doc/Luxuries.md
@@ -0,0 +1,54 @@
+# Luxuswaren
+
+In allen von Bauern bewohnten Regionen wird von diesen ein Luxusgut produziert.
+
+## Voraussetzungen
+
+### Balsam
+
+Balsam ist das weiterverarbeitete Harz der Balsambäume, die in tropischen
+Wäldern wachsen.
+
+### Gewürze
+
+Gewürze werden aus einer Vielzahl von Pflanzen gewonnen, die vor allem in
+tropischen oder subtropischen Regionen gedeihen.
+
+### Juwelen
+
+Juwelen werden von den Bauern in hoch gelegenen Ländern, in Gebirgen und auf
+Gletschern gefördert.
+
+### Myrrhe
+
+Myrrhe ist das getrocknete Harz des Myrrhenstrauches, der in trockenen warmen
+Gegenden wächst.
+
+### Öl
+
+Öle gewinnen die Bauern aus ölhaltigen Pflanzen wie dem Ölbaum oder dem
+Olivenbaum. Diese wachsen in wärmeren Regionen.
+
+### Pelze
+
+Pelze stammen von allerlei Tieren von überall auf Lemuria.
+
+### Seide
+
+Seide wird aus dem Sekret der Seidenraupe gewonnen, welche wiederum die
+Maulbeere als Nahrungsquelle benötigt, die in gemäßigtem Klima gedeiht.
+
+### Weihrauch
+
+## Vorkommen
+
+| Luxusgut      | Landschaften                          | Höhe      | Temperatur | Bäume  |
+|---------------|---------------------------------------|-----------|------------|--------|
+| **Balsam**    | Ebene, Gebirge, Hochland, Sumpf, Wald |           | ≥ 20 °C    | ≥ 500  |
+| **Gewürze**   | Ebene, Hochland, Wüste                |           | ≥ 20 °C    | < 300  |
+| **Juwelen**   | Gebirge, Gletscher, Hochland          | ≥ 650 m   |            |        |
+| **Myrrhe**    | Ebene, Gebirge, Hochland, Wüste       | 350–800 m | ≥ 15 °C    | < 500  |
+| **Öl**        | Ebene, Gebirge, Hochland, Wüste       |           | ≥ 15 °C    |        |
+| **Pelze**     |                                       |           |            |        |
+| **Seide**     | Ebene, Hochland, Sumpf, Wald          | < 650 m   | 5–15 °C    | < 300  |
+| **Weihrauch** | Gebirge, Hochland, Wüste              |           |            | < 300  |
--- src/Map/Converter.php
+++ src/Map/Converter.php
@@ -4,6 +4,20 @@ namespace Lemuria\Alpha\Map;
 
 use Lemuria\Alpha\Map\Exception\MissingRegionException;
 use Lemuria\Alpha\Map\Exception\RegionTypeException;
+use Lemuria\Engine\Fantasya\Factory\Workplaces;
+use Lemuria\Exception\LemuriaException;
+use Lemuria\Lemuria;
+use Lemuria\Model\Dictionary;
+use Lemuria\Model\Domain;
+use Lemuria\Model\Fantasya\Commodity\Camel;
+use Lemuria\Model\Fantasya\Commodity\Elephant;
+use Lemuria\Model\Fantasya\Commodity\Griffin;
+use Lemuria\Model\Fantasya\Commodity\Horse;
+use Lemuria\Model\Fantasya\Commodity\Iron;
+use Lemuria\Model\Fantasya\Commodity\Peasant;
+use Lemuria\Model\Fantasya\Commodity\Silver;
+use Lemuria\Model\Fantasya\Commodity\Stone;
+use Lemuria\Model\Fantasya\Commodity\Wood;
 use Lemuria\Model\Fantasya\Factory\BuilderTrait;
 use Lemuria\Model\Fantasya\Landscape\Desert;
 use Lemuria\Model\Fantasya\Landscape\Forest;
@@ -13,9 +27,13 @@ use Lemuria\Model\Fantasya\Landscape\Mountain;
 use Lemuria\Model\Fantasya\Landscape\Ocean;
 use Lemuria\Model\Fantasya\Landscape\Plain;
 use Lemuria\Model\Fantasya\Landscape\Swamp;
+use Lemuria\Model\Fantasya\Quantity;
 use Lemuria\Model\Fantasya\Region;
+use Lemuria\Singleton;
 use Lemuria\Tools\Lemuria\Area;
+use Lemuria\Tools\Lemuria\Good;
 use Lemuria\Tools\Lemuria\Map;
+use Lemuria\Tools\Lemuria\MapConfig;
 use Lemuria\Tools\Lemuria\Moisture;
 use Lemuria\Tools\Lemuria\Terrain;
 
@@ -23,10 +41,30 @@ class Converter
 {
 	use BuilderTrait;
 
+	protected const WORKPLACES = 10000;
+
+	protected const MAX_SILVER = 6 * 3 * 10;
+
+	protected const MAX_STONE = 130;
+
+	protected const MAX_IRON = 110;
+
+	protected const MAX_GRIFFINS = 30;
+
+	protected const MIN_RESOURCES = 0.33;
+
+	protected const MAX_GLACIER = 0.45;
+
 	protected Map $map;
 
-	public function __construct(Map &$map) {
-		$this->map = &$map;
+	protected Dictionary $dictionary;
+
+	protected float $maximum;
+
+	public function __construct(protected MapConfig $config, Map &$map) {
+		$this->map        = &$map;
+		$this->dictionary = new Dictionary();
+		$this->maximum    = $config->square * 100.0;
 	}
 
 	public function createRegion(int $x, int $y): Region {
@@ -35,8 +73,34 @@ class Converter
 			throw new MissingRegionException($x, $y);
 		}
 
-		$type      = $data[Map::VEGETATION] ?? 0;
-		$landscape = match ($type) {
+		$id        = Lemuria::Catalog()->nextId(Domain::LOCATION);
+		$landscape = $this->getLandscape($x, $y, $data[Map::VEGETATION] ?? 0);
+		$peasants  = $this->calculatePeasants($landscape, $data[Map::GOOD]);
+		$animal    = $this->getAnimal($landscape);
+
+		$region = new Region();
+		$region->setId($id);
+		$region->setLandscape(self::createLandscape($landscape));
+		if ($landscape === Ocean::class) {
+			$region->setName($this->dictionary->get('region.' . $landscape));
+		} else {
+			$region->setName($this->dictionary->get('region.' . $landscape) . ' ' . $id);
+			$this->addResources([
+				Peasant::class => $peasants,
+				Silver::class  => $this->calculateSilver($peasants, $data[Map::FERTILITY]),
+				Wood::class    => $this->calculateForest($data[Map::GOOD]),
+				Stone::class   => $this->calculateStone($landscape, $data),
+				Iron::class    => $this->calculateIron($landscape, $data),
+				$animal        => $this->calculateAnimals($animal, $data[Map::GOOD], $data[Map::ALTITUDE])
+			], $region);
+			$this->setLuxuries($region, $data);
+			$this->setHerbage($region, $landscape, $x, $y);
+		}
+		return $region;
+	}
+
+	protected function getLandscape(int $x, int $y, int $vegetation): string {
+		return match ($vegetation) {
 			Terrain::OCEAN,    Moisture::LAKE,        Area::ICE           => Ocean::class,
 			Terrain::PLAIN,    Area::TUNDRA                               => Plain::class,
 			Area::RAIN_FOREST                                             => Forest::class,
@@ -45,11 +109,95 @@ class Converter
 			Moisture::MOOR                                                => Swamp::class,
 			Moisture::OASIS,   Area::DESERT                               => Desert::class,
 			Area::GLACIER                                                 => Glacier::class,
-			default                                                       => throw new RegionTypeException($x, $y, $type)
+			default                                                       => throw new RegionTypeException($x, $y, $vegetation)
 		};
+	}
 
-		$region = new Region();
-		$region->setLandscape(self::createLandscape($landscape));
-		return $region;
+	protected function getAnimal(string $landscape): ?string {
+		return match ($landscape) {
+			Plain::class                   => Horse::class,
+			Highland::class, Desert::class => Camel::class,
+			Forest::class, Swamp::class    => Elephant::class,
+			Glacier::class                 => Griffin::class,
+			default                        => Singleton::class
+		};
+	}
+
+	protected function calculatePeasants(string $landscape, array $goods): int {
+		if ($landscape === Glacier::class) {
+			return 0;
+		}
+
+		$fishing  = $goods[Good::FISH] / ($this->maximum / $this->config->hunting);
+		$farming  = $goods[Good::CROP] / ($this->maximum / $this->config->farming);
+		$breeding = $goods[Good::MEAT] / ($this->maximum / $this->config->breeding);
+		return (int)floor(($fishing + $farming + $breeding) * self::WORKPLACES);
+	}
+
+	protected function calculateSilver(int $peasants, float $fertility): int {
+		return (int)floor($peasants * self::MAX_SILVER * $fertility);
+	}
+
+	protected function calculateForest(array $goods): int {
+		$forest = $goods[Good::WOOD] / ($this->maximum / $this->config->forestry);
+		return (int)floor($forest * self::WORKPLACES / Workplaces::TREE);
+	}
+
+	protected function calculateStone(string $landscape, array $data): int {
+		$difference = max(0, $data[Map::ALTITUDE] - $this->config->highland);
+		$maximum    = $this->config->maxHeight + $this->config->maxDiff - $this->config->highland;
+		$percentage = 1.0 - min($difference / $maximum, 1.0);
+		$minimum    = self::MIN_RESOURCES * self::MAX_STONE;
+		$stone      = $minimum + $percentage * (self::MAX_STONE - $minimum);
+		if ($landscape === Glacier::class) {
+			$stone *= self::MAX_GLACIER;
+		}
+		return (int)floor($stone);
+	}
+
+	protected function calculateIron(string $landscape, array $data): int {
+		$difference = max(0, $data[Map::ALTITUDE] - $this->config->highland);
+		$maximum    = $this->config->maxHeight + $this->config->maxDiff - $this->config->highland;
+		$percentage = min($difference / $maximum, 1.0);
+		$minimum    = self::MIN_RESOURCES * self::MAX_IRON;
+		$stone      = $minimum + $percentage * (self::MAX_IRON - $minimum);
+		if ($landscape === Glacier::class) {
+			$stone *= self::MAX_GLACIER;
+		}
+		return (int)floor($stone);
+	}
+
+	protected function calculateAnimals(string $animal, array $goods, int $altitude): int {
+		if ($animal === Griffin::class) {
+			$difference = max(0, $altitude - $this->config->mountain);
+			$maximum    = $this->config->maxHeight + $this->config->maxDiff;
+			$percentage = min($difference / $maximum, 1.0);
+			return $percentage * self::MAX_GRIFFINS;
+		}
+
+		$animals    = $goods[Good::GAME] / ($this->maximum / $this->config->hunting);
+		$workplaces = match ($animal) {
+			Horse::class    => Workplaces::HORSE,
+			Camel::class    => Workplaces::CAMEL,
+			Elephant::class => Workplaces::ELEPHANT,
+			default         => null
+		};
+		return $workplaces ? (int)floor($animals * self::WORKPLACES / $workplaces) : 0;
+	}
+
+	protected function addResources(array $resources, Region $region): void {
+		foreach ($resources as $class => $count) {
+			if ($count > 0) {
+				$region->Resources()->add(new Quantity(self::createCommodity($class), $count));
+			}
+		}
+	}
+
+	protected function setLuxuries(Region $region, array $data): void {
+		throw new LemuriaException('Not implemented yet.');
+	}
+
+	protected function setHerbage(Region $region, string $landscape, int $x, int $y): void {
+		throw new LemuriaException('Not implemented yet.');
 	}
 }
