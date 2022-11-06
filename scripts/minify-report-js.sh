ecb48a2e2d2fc18c2f96bffd84293512c5faf51d
lemuria-pbem <lemuria@online.de>
2022-11-06 14:00:06 +0100

    [FEATURE] Migrate to Bootstrap 5.

--- scripts/minify-report-js.sh
+++ scripts/minify-report-js.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-jquery=vendor/components/jquery/jquery.min.js
 bootstrap=vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js
 source=web/js/script.js
 target=web/js/report.min.js
 
 rm -f $target
-cp $jquery $target
-cat $bootstrap >> $target
+cp $bootstrap $target
 echo >> $target
 head -n 1 $source >> $target
 php scripts/minify-javascript.php $source >> $target
--- web/index.php
+++ web/index.php
@@ -14,7 +14,6 @@ $fantasya = new FantasyaGame();
 		<title>Lemuria-Auswertung</title>
 		<link rel="stylesheet" href="/css/bootstrap.css"/>
 		<link rel="stylesheet" href="/css/style.css"/>
-		<script defer type="text/javascript" src="/js/jquery.min.js"></script>
 		<script defer type="text/javascript" src="/js/bootstrap.bundle.min.js"></script>
 		<script defer type="text/javascript" src="/js/script.js"></script>
 	</head>
