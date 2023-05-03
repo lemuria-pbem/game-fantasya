874bb998f0c13051cae8a33ed7f51567ba34b84b
lemuria-pbem <lemuria@online.de>
2023-05-03 08:38:08 +0200

    [FEATURE] Add debugging with configurable cherry-picking for moves and command priorities

--- bin/examples/debug-config.php
+++ bin/examples/debug-config.php
@@ -6,3 +6,7 @@ use Lemuria\Engine\Fantasya\Turn\SelectiveCherryPicker;
 function configure(SelectiveCherryPicker $cherryPicker): void {
 	$cherryPicker->everyone()->everything();
 }
+
+function modifyAnythingForDebugging(): void {
+	// Modify anything here that is needed for debugging.
+}
