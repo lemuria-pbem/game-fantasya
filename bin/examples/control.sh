9976ade013ea714250059ffce86b87ac7138f166
lemuria-pbem <lemuria@online.de>
2023-07-25 08:44:45 +0200

    [BUGFIX] Fix example control script.

--- bin/examples/control.sh
+++ bin/examples/control.sh
@@ -2,15 +2,15 @@
 
 PROJECT=/home/lemuria/game-fantasya
 OUTPUT=/tmp/control-lemuria.txt
-FROM="'From: user@host'"
-RECIPIENT="'mail@example.org'"
-SUBJECT="'Lemuria report error'"
+FROM="From: user@host"
+RECIPIENT=mail@example.org
+SUBJECT="Lemuria report error"
 
 cd $PROJECT > $OUTPUT 2>&1
 bin/sync.sh -o >> $OUTPUT 2>&1
 php $PROJECT/bin/turn.php >> $OUTPUT 2>&1
 if [ $? -ne 0 ]
 then
-	mail -s $SUBJECT -a $FROM $RECIPIENT < $OUTPUT
+	mail -s "$SUBJECT" -a "$FROM" $RECIPIENT < $OUTPUT
 fi
 rm $OUTPUT
