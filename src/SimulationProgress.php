d7c1be06f3e25b702209c4d5d022f3edf0632743
lemuria-pbem <lemuria@online.de>
2023-08-14 07:54:23 +0200

    [FEATURE] Add maintenance and support events to simulation.

--- src/SimulationProgress.php
+++ src/SimulationProgress.php
@@ -3,11 +3,13 @@ declare(strict_types = 1);
 namespace Lemuria\Game\Fantasya;
 
 use Lemuria\Engine\Fantasya\Event\Layabout;
+use Lemuria\Engine\Fantasya\Event\Simulation\Maintenance;
+use Lemuria\Engine\Fantasya\Event\Simulation\Support;
 use Lemuria\Engine\Fantasya\Factory\DefaultProgress;
 
 final class SimulationProgress extends DefaultProgress
 {
-	public final const EVENTS = [Layabout::class];
+	public final const EVENTS = [Maintenance::class, Support::class, Layabout::class];
 
 	/**
 	 * @return array<string>
